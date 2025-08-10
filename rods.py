f23ed802a0abf4ec4c35faca971dee15c1ee0896
wedward <wehosea@gmail.com>
2025-08-10 15:26:22 -0500

    fdas

--- rods.py
+++ rods.py
@@ -8,24 +8,25 @@ extend = 2
 make_cube = False
 
 octarod = RegularPolygon(.25, 8 , rotation=45/2, major_radius=False)
-hexarod =   RegularPolygon(.2, 6, rotation=30, major_radius=False)
-
+hexarod =   RegularPolygon(.3, 6, rotation=30, major_radius=False)
+quadrod = RegularPolygon(.2, 4, False)
+cyl = Circle(.5)
 rod_shape = hexarod
 
 #build 3-axis
-def build3(rods=rods, extend=extend, make_cube=make_cube, rod_shape=rod_shape):
-    pts = GridLocations(1,1,rods,rods)
+def build3(rods=3, extend=2,  rod_shape=octarod, spacing= 1.0, make_cube=False):
+    pts = GridLocations(1.0*spacing,1.0*spacing,rods,rods)
     objs = pts * rod_shape
     for obj in objs[1:]:
         objs[0]+= obj
 
-    z = extrude( objs[0], amount= (rods/2) + extend, both=True) 
+    z = extrude( objs[0], amount= (rods/2) + extend + spacing, both=True) 
     z.color=Color('blue')
 
-    y = z.translate((.5,0,0)).rotate(Axis.X, 90)
+    y = z.translate((0.5*spacing,0,0)).rotate(Axis.X, 90)
     y.color = Color('lawngreen')
 
-    x = z.translate((.5,.5,0)).rotate(Axis.Y, 90)
+    x = z.translate((0.5*spacing,0.5*spacing,0)).rotate(Axis.Y, 90)
     x.color = Color('red')
 
     if make_cube:
@@ -33,11 +34,11 @@ def build3(rods=rods, extend=extend, make_cube=make_cube, rod_shape=rod_shape):
         return Compound([obj & cube for obj in [x,y,z]] )
     
     else:
-        return   [x,y,z] 
+        return   [x,y,z]
 
 
 
-def tetra(planes=False):
+def tetra(planes=True):
     tetrahedron_vertices = [(1, 1, 1), (1, -1, -1), (-1, 1, -1), (-1, -1, 1)]
     hull = ConvexHull(tetrahedron_vertices).simplices.tolist()
     platonic_faces = []
@@ -53,31 +54,52 @@ def tetra(planes=False):
     return Solid(Shell(platonic_faces))
 
 #build 4-axis
-def build():
+def build4(rods=3, extend=2.0, rod_shape=hexarod, spacing=1):
 
-    planes = tetra(planes=True)
+    planes = tetra()
 
-    pts = HexLocations(.5,rods-rods%2,rods)
+    pts = HexLocations(1* spacing,rods-rods%2,rods)
 
-    p0 = extrude(Compound(planes[0] * pts * rod_shape), extend+rods/2, both=True)
-    p1 = extrude(Compound(planes[1] * Pos(3**(0.5)/4,-.25,0) * pts  * rod_shape), extend+rods/2, both=True)
-    p2 = extrude(Compound(planes[2] * Pos(0,.5,0) * pts * rod_shape), extend+rods/2, both=True)
-    p3 = extrude(Compound(planes[3] * Pos(3**.5/4,.25,0) * pts * rod_shape), extend+rods/2, both=True)
+    amt = (rods/2) + extend + spacing + 1
 
-    r =planes[2]  * Rectangle(3,5)
+    p0 = extrude(Compound(planes[0] * Pos(3**(0.5)/2 * spacing, 0,0) * pts * rod_shape), amt, both=True)
+    p1 = extrude(Compound(planes[1] * Pos(3**(0.5) * spacing,-.5 * spacing,0) * pts  * rod_shape), amt, both=True)
+    p2 = extrude(Compound(planes[2] * Pos(0,.5 * spacing,0) * pts * rod_shape), amt, both=True)
+    p3 = extrude(Compound(planes[3] * Pos(3**.5/2 * spacing, 1  * spacing,0) * pts * rod_shape), amt, both=True)
 
-    p0.color = Color('cyan')
+
+
+    p0.color = Color('magenta')
     p1.color = Color('gray')
-    p2.color = Color('magenta')
-    p3.color = Color('yellow')
+    p2.color = Color('yellow')
+    p3.color = Color('cyan')
 
    
-    return [p0,p1, p2, p3]
+    return [p0,p1, p2, p3 , ]
+
+
+def build(axes):
+
+    ## spacing = 1.63 ??
+
+    match axes:
+        case 3:
+            show(*build3())
+        case 4:
+            show(*build4())
+        case 7:
+            show(*build4(spacing=1.63), *build3(rod_shape=quadrod, spacing=2) )
+        case _:
+            print('invalid')
+
+    
+    
+
 
 
 
 if __name__ == '__main__':
     # from make import make
-    # make(Compound(build()))
+    # make(Compound(build())) 
     
-    show(*build())
+    build(7)
